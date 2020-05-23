56a88015bf43c538d467e7ff5ba19a6ba891842a
Noah <hanlun.song@gmail.com>
2020-05-23 19:34:53 -0400

    add one more

---
+++ src/buy-sell-stock-2.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+  const numOfDays = prices.length
+  if (numOfDays <= 1) return 0
+  let profits = 0
+  for (let i = 0; i < numOfDays; ++i) {
+    if (prices[i + 1] > prices[i]) {
+      profits += prices[i + 1] - prices[i]
+    }
+  }
+  return profits
+}
+
+console.log(maxProfix([7, 1, 5, 3, 6, 4]))
+console.log(maxProfix([1, 2, 3, 4, 5]))
+console.log(maxProfix([7, 6, 4, 3, 1]))
