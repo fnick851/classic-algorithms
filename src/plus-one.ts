cdda71826f5a4e8e7b13ae7af7234770f876d1ff
Noah <hanlun.song@gmail.com>
2020-05-28 15:46:35 -0400

    improve plus-one

--- src/plus-one.ts
+++ src/plus-one.ts
@@ -1,8 +1,32 @@
-const plusOne = (digits: number[]): number[] =>
-  (BigInt(digits.join("")) + 1n)
-    .toString()
-    .split("")
-    .map(int => parseInt(int))
+// /// method 1
+// const plusOne = (digits: number[]): number[] =>
+//   (BigInt(digits.join("")) + 1n)
+//     .toString()
+//     .split("")
+//     .map(int => parseInt(int))
+
+/// method 2
+/// per digit operation, time O(n). space O(1)
+function plusOne(digits: number[]): number[] {
+  const n = digits.length
+
+  for (let idx = n - 1; idx >= 0; --idx) {
+    // set all the nines at the end of array to zeros
+    if (digits[idx] == 9) {
+      digits[idx] = 0
+    }
+    // here we have the rightmost not-nine
+    else {
+      // increase this rightmost not-nine by 1
+      digits[idx]++
+      // and the job is done
+      return digits
+    }
+  }
+  // we're here because all the digits are nines
+  digits.unshift(1)
+  return digits
+}
 
 console.log(plusOne([1, 2, 3]))
 console.log(plusOne([4, 3, 2, 1]))
